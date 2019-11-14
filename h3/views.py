df2d7bbebd7189dc06de622bb6fb6b535579165a
Peyton Duncan <peyton.duncan45@gmail.com>
2019-11-13 19:52:35 -0500

    Added Static Files and Admin Page

--- h3/views.py
+++ h3/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import View
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
@@ -13,23 +13,21 @@ class LoginView(View):
     template_name = 'login.html'
 
     def get(self, request):
+        # form = User
         return render(request, self.template_name)
     
     def post(self, request):
         return render(request, self.template_name)
 
 class LogoutView(View):
-    template_name = 'login.html'
+    # template_name = 'login.html'
 
     def get(self, request):
-        return render(request, self.template_name)
-    
-    def post(self, request):
-        return render(request, self.template_name)
+        logout(request)
+        return redirect('/login')
 
 class SignupView(View):
     def post(self,request):
-    
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
@@ -39,6 +37,7 @@ class SignupView(View):
             login(request, user)
             return redirect('home')
         return render(request, 'signup.html', {'form': form})
+
     def get(self,request):
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form})
